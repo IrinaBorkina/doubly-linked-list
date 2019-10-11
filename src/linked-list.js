fdca64cb5c27b8e1816f9aad48534b657e3fa355
Irina <irina_4ever@mail.ru>
2019-10-11 13:56:28 +0300

    feat: add a complete solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -31,43 +31,40 @@ class LinkedList {
     }
 
     at(index) {
-        if (index < 0 || this.length <= index) {
-            return -1;
-        }
-            let current = this._head;
-            let count = 0;
-
-            while (count < index) {
-                current = current.next;
-                count++;
-            }
-            return current.data;
-    }
-
-    insertAt(index, data) {
-        if (index < 0 || this.length < index) {
+        if (index < 0 || this.length === 0|| this.length < index) {
             return false;
         }
 
-        let node = new Node( data );
+        let current = this._head;
+        let count = 0;
 
-        if( index === 0 ) {
-            node.next = this._head;
+        while (count < index) {
+            current = current.next;
+            count++;
+        }
+        return current.data;
+    }
+
+    insertAt(index, data) {
+        if (index < 0 || this.length === 0|| this.length < index) {
+            return false;
+        }
+
+        let node = new Node(data);
+
+        if (index === 0) {
             this._head.prev = node;
+            node.next = this._head;
             this._head = node;
-        } else if (index === this.length) {
-            this._tail.next = node;
-            node.prev = this._tail;
-            this._tail = node;
         } else {
             let current = this._head;
             let prev = null;
-            let counter = 0;
+            let count = 0;
 
-            while(counter < index) {
+            while(count < index) {
                 prev = current;
                 current = current.next;
-                counter++;
+                count++;
             }
 
             prev.next = node;
@@ -76,7 +73,8 @@ class LinkedList {
             node.next = current;
             current.prev = node;
         }
-            this.length++;
+        this.length++;
+        return this;
     }
     
 
@@ -91,17 +89,46 @@ class LinkedList {
 
     clear() {
         this._head.data = null;
-		this._tail.data = null;
-		this.length = 0;
-		return this;
+        this._tail.data = null;
+        this.length = 0;
+        return this;
     }
 
     deleteAt(index) {
-        
+        let current = this._head;
+
+        while (index--) {
+            current = current.next;
+        }
+
+        if (current.prev) {
+            current.prev.next = current.next;
+        }
+
+        if (current.next) {
+            current.next.prev = current.prev;
+        }
+
+        return this;
     }
 
     reverse() {
+        this._tail = this._head;
 
+        while (this._head !== null) {
+            let temp = this._head.prev;
+
+            this._head.prev = this._head.next;
+            this._head.next = temp;
+
+            if (this._head.prev !== null) {
+                this._head = this._head.prev;
+            } else {
+                break;
+            }
+        }
+
+        return this;
     }
 
     indexOf(data) {
@@ -115,7 +142,8 @@ class LinkedList {
             
             element = element.next;
             index++;
-        }		
+        }
+        
 		return -1;
     }
 }
